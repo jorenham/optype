620d10ac89e09a6534d35a3325236b8e1a4f3599
jorenham <jhammudoglu@gmail.com>
2024-02-26 12:31:19 +0100

    some tests `Does*` and `do_*`

--- tests/test_protocols.py
+++ tests/test_protocols.py
@@ -7,6 +7,8 @@ import pytest
 
 import optype
 import optype._can
+import optype._do
+import optype._does
 import optype._has
 
 
@@ -104,6 +106,15 @@ def _get_protocols(module: ModuleType) -> frozenset[type]:
     })
 
 
+def _get_callable_members(module: ModuleType) -> frozenset[str]:
+    """Return the public protocol types within the given module."""
+    return frozenset({
+        name for name in dir(module)
+        if not name.startswith('_')
+        and callable(getattr(module, name))
+    })
+
+
 def test_all_public():
     """
     Ensure all of protocols in `optype._can` and `optype._has` are in
@@ -112,20 +123,54 @@ def test_all_public():
     protocols_all = _get_protocols(optype)
     protocols_can = _get_protocols(optype._can)
     protocols_has = _get_protocols(optype._has)
+    protocols_does = _get_protocols(optype._does)
 
-    assert protocols_can | protocols_has == protocols_all
+    assert protocols_can | protocols_has | protocols_does == protocols_all
 
 
-@pytest.mark.parametrize('cls', _get_protocols(optype))
-def test_instance_checkable(cls: type):
-    """Ensure all optype protocols are `@runtime_checkable`."""
+@pytest.mark.parametrize('cls', _get_protocols(optype._can))
+def test_can_runtime_checkable(cls: type):
+    """Ensure that all `Can*` protocols are `@runtime_checkable`."""
     assert _is_runtime_protocol(cls)
 
 
-@pytest.mark.parametrize('cls', _get_protocols(optype))
+@pytest.mark.parametrize('cls', _get_protocols(optype._has))
+def test_has_runtime_checkable(cls: type):
+    """Ensure that all `Has*` protocols are `@runtime_checkable`."""
+    assert _is_runtime_protocol(cls)
+
+
+@pytest.mark.parametrize('cls', _get_protocols(optype._does))
+def test_does_not_runtime_checkable(cls: type):
+    """Ensure that all `Does*` protocols are **not** `@runtime_checkable`."""
+    assert not _is_runtime_protocol(cls)
+
+
+def test_num_does_eq_num_do():
+    num_does = len(_get_protocols(optype._does))
+    num_do = len(_get_callable_members(optype._do))
+    assert num_does == num_do
+
+
+@pytest.mark.parametrize('cls', _get_protocols(optype._does))
+def test_does_has_do(cls: type):
+    """Ensure that all `Does*` protocols have a corresponding `do_` op."""
+    name = cls.__name__.removeprefix('Does')
+    assert name != cls.__name__
+
+    do_name = f'do_{name.lower()}'
+    do_op = getattr(optype._do, do_name, None)
+    assert do_op is not None, do_name
+    assert callable(do_op), do_name
+
+
+@pytest.mark.parametrize(
+    'cls',
+    _get_protocols(optype._can) | _get_protocols(optype._has),
+)
 def test_name_matches_dunder(cls: type):
     """
-    Ensure that each single-member optype name matches its member,
+    Ensure that each single-member `Can*` and `Has*` name matches its member,
     and that each multi-member optype does not have more members than it has
     super optypes.
     """
