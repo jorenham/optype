fa42a0a44ace3e50f7f92e12d9c7962befe734c4
jorenham <jhammudoglu@gmail.com>
2024-04-15 05:35:37 +0200

    clean the 'twice' example, split into cells

--- examples/twice.py
+++ examples/twice.py
@@ -1,25 +1,36 @@
-import typing
-import optype
+# %%
+from typing import (
+    Generic,
+    Literal,
+    TypeAlias,
+    TypeVar,
+    TypeVarTuple,
+    assert_type,
+    final,
+)
+from optype import CanMul, CanRMul
 
-Two: typing.TypeAlias = typing.Literal[2]
-Y = typing.TypeVar('Y')
+Y = TypeVar('Y')
+Two: TypeAlias = Literal[2]
 
 
-def twice(x: optype.CanRMul[Two, Y], /) -> Y:
+def twice(x: CanRMul[Two, Y], /) -> Y:
     return 2 * x
 
 
-typing.assert_type(twice(True), int)
-typing.assert_type(twice(1 / 137), float)
-typing.assert_type(twice(str(-1 / 12)), str)
-typing.assert_type(twice([object()]), list[object])
+# %%
+assert_type(twice(True), int)
+assert_type(twice(1 / 137), float)
+assert_type(twice(str(-1 / 12)), str)
+assert_type(twice([object()]), list[object])
 
 
-Ts = typing.TypeVarTuple('Ts')
+# %%
+Ts = TypeVarTuple('Ts')
 
 
-@typing.final
-class RMulArgs(typing.Generic[*Ts]):
+@final
+class RMulArgs(Generic[*Ts]):
     def __init__(self, *args: *Ts) -> None:
         self.args = args
 
@@ -29,27 +40,29 @@ class RMulArgs(typing.Generic[*Ts]):
         return RMulArgs(*self.args, *self.args)
 
 
-typing.assert_type(twice(RMulArgs(42, True)), RMulArgs[int, bool, int, bool])
-
-###
+assert_type(twice(RMulArgs(42, True)), RMulArgs[int, bool, int, bool])
 
 
-def twice2(x: optype.CanRMul[Two, Y] | optype.CanMul[Two, Y], /) -> Y:
-    return 2 * x if isinstance(x, optype.CanRMul) else x * 2
+# %%
+def twice2(x: CanRMul[Two, Y] | CanMul[Two, Y], /) -> Y:
+    return 2 * x if isinstance(x, CanRMul) else x * 2
 
 
+# %%
 class RMulThing:
     def __rmul__(self, y: Two, /) -> str:
         return f'{y} * _'
 
 
+assert_type(twice2(RMulThing()), str)
+assert twice2(RMulThing()) == '2 * _'
+
+
+# %%
 class MulThing:
     def __mul__(self, y: Two, /) -> str:
         return f'_ * {y}'
 
 
-typing.assert_type(twice2(RMulThing()), str)
-assert twice2(RMulThing()) == '2 * _'
-
-typing.assert_type(twice2(MulThing()), str)
+assert_type(twice2(MulThing()), str)
 assert twice2(MulThing()) == '_ * 2'
