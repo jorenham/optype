3928ce282e325e7b1e1021b244d76e661f6097b4
jorenham <jhammudoglu@gmail.com>
2024-06-29 05:49:34 +0200

    internal helper module for numpy 1/2 compat

---
+++ optype/numpy/_compat.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+
+from typing import Final, TypeAlias
+
+import numpy as np
+
+
+__all__ = 'Bool', 'Long', 'ULong'
+
+
+_NP_V2: Final[bool] = np.__version__.startswith('2.')
+
+
+if _NP_V2:
+    Bool: TypeAlias = np.bool
+    ULong: TypeAlias = np.ulong
+    Long: TypeAlias = np.long
+else:
+    Bool: TypeAlias = np.bool_
+    ULong: TypeAlias = np.uint
+    Long: TypeAlias = np.int_
