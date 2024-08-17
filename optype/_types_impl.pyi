df6cff8562fe6fe0d3298b6c16c1668b0b0d38a4
jorenham <jhammudoglu@gmail.com>
2024-08-17 03:35:45 +0200

    Improved `optype.types.AnnotatedAlias` type stubs

--- optype/_types_impl.pyi
+++ optype/_types_impl.pyi
@@ -2,9 +2,7 @@ import sys
 import types as _types
 from collections.abc import Iterator
 from typing import (
-    Any,
     Generic,
-    Literal,
     ParamSpec,
     TypeAlias,
     _SpecialForm,  # pyright: ignore[reportPrivateUsage]
@@ -12,28 +10,35 @@ from typing import (
     type_check_only,
 )
 
-from typing_extensions import Self, TypeVar, TypeVarTuple, override
+from typing_extensions import (
+    Self,
+    TypeAliasType,
+    TypeVar,
+    TypeVarTuple,
+    Unpack,
+    override,
+)
 
 __all__ = 'AnnotatedAlias', 'GenericType', 'LiteralAlias', 'UnionAlias'
 
-_TypeOrigin: TypeAlias = (
-    type | _types.GenericAlias | GenericType
+_TypeExpr: TypeAlias = type | _types.GenericAlias | GenericType | TypeAliasType
+_TypeParam: TypeAlias = (
+    TypeVar | ParamSpec | TypeVarTuple | UnpackAlias[tuple[object, ...]]
 )
-_TypeParam: TypeAlias = TypeVar | TypeVarTuple | ParamSpec
 
 # represents `typing._GenericAlias`
 # NOTE: This is different from `typing.GenericAlias`!
 class GenericType:
     @property
-    def __origin__(self) -> _TypeOrigin: ...
+    def __origin__(self, /) -> _TypeExpr | _SpecialForm: ...
     @property
-    def __args__(self) -> tuple[Any, ...]: ...
+    def __args__(self, /) -> tuple[object, ...]: ...
     @property
-    def __parameters__(self) -> tuple[_TypeParam, ...]: ...
+    def __parameters__(self, /) -> tuple[_TypeParam, ...]: ...
 
     def __init__(
         self,
-        origin: _TypeOrigin,
+        origin: _TypeExpr | _SpecialForm,
         args: tuple[object, ...] | object,
         /,
     ) -> None: ...
@@ -50,30 +55,36 @@ class GenericType:
     def __instancecheck__(self, obj: object, /) -> bool: ...
 
 @final
-class AnnotatedAlias(GenericType): ...
-@final
 class LiteralAlias(GenericType): ...
 @final
 class UnionAlias(GenericType): ...
 
-_Ts_co = TypeVar('_Ts_co', covariant=True, bound=tuple[object, ...] | TypeVarTuple)
+@final
+class AnnotatedAlias(GenericType):
+    @property
+    @override
+    def __origin__(self, /) -> _TypeExpr: ...
+    @property
+    def __metadata__(self, /) -> tuple[object, Unpack[tuple[object, ...]]]: ...
+
+_Ts_co = TypeVar('_Ts_co', bound=tuple[object, ...] | TypeVarTuple, covariant=True)
 
 @type_check_only
 class UnpackAlias(GenericType, Generic[_Ts_co]):
     @property
-    def __typing_unpacked_tuple_args__(self) -> tuple[Any, ...] | None: ...
+    def __typing_unpacked_tuple_args__(self, /) -> tuple[object, ...] | None: ...
     @property
-    def __typing_is_unpacked_typevartuple__(self) -> Literal[False, True]: ...
+    def __typing_is_unpacked_typevartuple__(self, /) -> bool: ...
 
     @property
     @override
-    def __origin__(self) -> type[_SpecialForm]: ...
+    def __origin__(self, /) -> type[_SpecialForm]: ...
     @property
     @override
-    def __args__(self) -> tuple[_Ts_co]: ...
+    def __args__(self, /) -> tuple[_Ts_co]: ...
     @property
     @override
-    def __parameters__(self) -> tuple[()] | tuple[TypeVarTuple]: ...
+    def __parameters__(self, /) -> tuple[()] | tuple[TypeVarTuple]: ...
 
     @override
-    def __init__(self, origin: _SpecialForm, args: tuple[_Ts_co]) -> None: ...
+    def __init__(self, origin: _SpecialForm, args: tuple[_Ts_co], /) -> None: ...
