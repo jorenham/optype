a6ee452ab18283a3f486063adbedfd0337f1a18f
jorenham <jhammudoglu@gmail.com>
2024-11-09 18:40:36 +0100

    fix pyright error when attempting to import dask

--- optype/array_api/_device_dask.py
+++ optype/array_api/_device_dask.py
@@ -1,7 +1,9 @@
+# mypy: disable-error-code="import-untyped"
+# pyright: reportPrivateUsage=false, reportMissingTypeStubs=false
+# ruff: noqa: N812
+
 try:
-    from array_api_compat.common._helpers import (  # pyright: ignore[reportMissingTypeStubs]
-        _dask_device as _DeviceDask,  # pyright: ignore[reportUnknownVariableType]  # noqa: N812
-    )
+    from array_api_compat.common._helpers import _dask_device as _DeviceDask
 except ImportError:
     from typing import NoReturn as _DeviceDask
 
