1b93c2e458417ac762097d756394253aefee1684
jorenham <jhammudoglu@gmail.com>
2024-11-24 19:58:58 +0100

    reproduce #194

--- tests/numpy/test_to.pyi
+++ tests/numpy/test_to.pyi
@@ -29,26 +29,73 @@ si1: onp.ToInt = np.int_(42)
 sf0: onp.ToFloat = 42.0
 sf1: onp.ToFloat = np.longdouble(42.0)
 
-sc0: onp.ToComplex = 42.0 + 1j
+sc0: onp.ToComplex = 42 + 0j
 sc1: onp.ToComplex = np.clongdouble(42.0 + 1j)
 
 # vectors
-
 vb: onp.ToBool1D = vec_b
 vi: onp.ToInt1D = vec_i
 vf: onp.ToFloat1D = vec_f
 vc: onp.ToComplex1D = vec_c
 
-# matrices
+vsb: onp.ToBool1D = True  # type: ignore[assignment]  # pyright: ignore[reportAssignmentType]
+vsi: onp.ToInt1D = 42  # type: ignore[assignment]  # pyright: ignore[reportAssignmentType]
+vsf: onp.ToFloat1D = 42.0  # type: ignore[assignment]  # pyright: ignore[reportAssignmentType]
+vsc: onp.ToComplex1D = 42 + 1j  # type: ignore[assignment]  # pyright: ignore[reportAssignmentType]
 
+vub: onp.ToBool1D = "illegal"  # type: ignore[assignment]  # pyright: ignore[reportAssignmentType]
+vui: onp.ToInt1D = "illegal"  # type: ignore[assignment]  # pyright: ignore[reportAssignmentType]
+vuf: onp.ToFloat1D = "illegal"  # type: ignore[assignment]  # pyright: ignore[reportAssignmentType]
+vuc: onp.ToComplex1D = "illegal"  # type: ignore[assignment]  # pyright: ignore[reportAssignmentType]
+
+# matrices
 mb: onp.ToBool2D = mat_b
 mi: onp.ToInt2D = mat_i
 mf: onp.ToFloat2D = mat_f
 mc: onp.ToComplex2D = mat_c
 
-# tensors
+mvb: onp.ToBool2D = vec_b  # type: ignore[assignment]  # pyright: ignore[reportAssignmentType]
+mvi: onp.ToInt2D = vec_i  # type: ignore[assignment]  # pyright: ignore[reportAssignmentType]
+mvf: onp.ToFloat2D = vec_f  # type: ignore[assignment]  # pyright: ignore[reportAssignmentType]
+mvc: onp.ToComplex2D = vec_c  # type: ignore[assignment]  # pyright: ignore[reportAssignmentType]
 
+msb: onp.ToBool2D = True  # type: ignore[assignment]  # pyright: ignore[reportAssignmentType]
+msi: onp.ToInt2D = 42  # type: ignore[assignment]  # pyright: ignore[reportAssignmentType]
+msf: onp.ToFloat2D = 42.0  # type: ignore[assignment]  # pyright: ignore[reportAssignmentType]
+msc: onp.ToComplex2D = 42 + 0j  # type: ignore[assignment]  # pyright: ignore[reportAssignmentType]
+
+mub: onp.ToBool2D = "illegal"  # type: ignore[assignment]  # pyright: ignore[reportAssignmentType]
+mui: onp.ToInt2D = "illegal"  # type: ignore[assignment]  # pyright: ignore[reportAssignmentType]
+muf: onp.ToFloat2D = "illegal"  # type: ignore[assignment]  # pyright: ignore[reportAssignmentType]
+muc: onp.ToComplex2D = "illegal"  # type: ignore[assignment]  # pyright: ignore[reportAssignmentType]
+
+# tensors
 tb: onp.ToBoolND = arr_b
 ti: onp.ToIntND = arr_i
 tf: onp.ToFloatND = arr_f
 tc: onp.ToComplexND = arr_c
+
+tmb: onp.ToBoolND = mat_b
+tmi: onp.ToIntND = mat_i
+tmf: onp.ToFloatND = mat_f
+tmc: onp.ToComplexND = mat_c
+
+tvb: onp.ToBoolND = vec_b
+tvi: onp.ToIntND = vec_i
+tvf: onp.ToFloatND = vec_f
+tvc: onp.ToComplexND = vec_c
+
+tsb: onp.ToBoolND = True  # type: ignore[assignment]  # pyright: ignore[reportAssignmentType]
+tsi: onp.ToIntND = 42  # type: ignore[assignment]  # pyright: ignore[reportAssignmentType]
+tsf: onp.ToFloatND = 42.0  # type: ignore[assignment]  # pyright: ignore[reportAssignmentType]
+tsc: onp.ToComplexND = 42 + 0j  # type: ignore[assignment]  # pyright: ignore[reportAssignmentType]
+
+tob: onp.ToBoolND = object()  # type: ignore[assignment]  # pyright: ignore[reportAssignmentType]
+toi: onp.ToIntND = object()  # type: ignore[assignment]  # pyright: ignore[reportAssignmentType]
+tof: onp.ToFloatND = object()  # type: ignore[assignment]  # pyright: ignore[reportAssignmentType]
+toc: onp.ToComplexND = object()  # type: ignore[assignment]  # pyright: ignore[reportAssignmentType]
+
+tub: onp.ToBoolND = "illegal"  # type: ignore[assignment]  # pyright: ignore[reportAssignmentType]
+tui: onp.ToIntND = "illegal"  # type: ignore[assignment]  # pyright: ignore[reportAssignmentType]
+tuf: onp.ToFloatND = "illegal"  # type: ignore[assignment]  # pyright: ignore[reportAssignmentType]
+tuc: onp.ToComplexND = "illegal"  # type: ignore[assignment]  # pyright: ignore[reportAssignmentType]
