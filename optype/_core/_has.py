a8b796fb312707ba4b7a1ff36059a2a3fbee2e94
jorenham <jhammudoglu@gmail.com>
2025-01-19 16:10:38 +0100

    change `HasWrapped` to have a single covariant callable type param

--- optype/_core/_has.py
+++ optype/_core/_has.py
@@ -8,7 +8,6 @@ from typing import Any, ClassVar, Protocol, TypeAlias
 if sys.version_info >= (3, 13):
     from typing import (
         LiteralString,
-        ParamSpec,
         TypeVar,
         TypeVarTuple,
         Unpack,
@@ -18,7 +17,6 @@ if sys.version_info >= (3, 13):
 else:
     from typing_extensions import (
         LiteralString,
-        ParamSpec,
         TypeVar,
         TypeVarTuple,
         Unpack,
@@ -53,12 +51,9 @@ def __dir__() -> list[str]:
 
 
 _Ts = TypeVarTuple("_Ts")
-_Tss = ParamSpec("_Tss")
 _TypeT = TypeVar("_TypeT", bound=type)
-_FT_co = TypeVar("_FT_co", bound=Callable[..., object], covariant=True)
-
-_T_co = TypeVar("_T_co", covariant=True)
 _ObjectT_co = TypeVar("_ObjectT_co", default=object, covariant=True)
+_FuncT_co = TypeVar("_FuncT_co", bound=Callable[..., object], covariant=True)
 
 __AnyMapping: TypeAlias = "Mapping[str, object]"
 __AnyDict: TypeAlias = dict[str, Any]  # pyright: ignore[reportExplicitAny]
@@ -183,15 +178,15 @@ class HasTypeParams(Protocol[Unpack[_Ts]]):
 
 
 @runtime_checkable
-class HasFunc(Protocol[_FT_co]):
+class HasFunc(Protocol[_FuncT_co]):
     @property
-    def __func__(self, /) -> _FT_co: ...
+    def __func__(self, /) -> _FuncT_co: ...
 
 
 @runtime_checkable
-class HasWrapped(Protocol[_Tss, _T_co]):
+class HasWrapped(Protocol[_FuncT_co]):
     @property
-    def __wrapped__(self, /) -> Callable[_Tss, _T_co]: ...
+    def __wrapped__(self, /) -> _FuncT_co: ...
 
 
 @runtime_checkable
