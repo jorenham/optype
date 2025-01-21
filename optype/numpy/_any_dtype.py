7038c57bb54e9879d0febf2fed5dc47ec5946caf
jorenham <jhammudoglu@gmail.com>
2025-01-21 05:01:50 +0100

    accept structured dtype specs in `onp.Any[Void]DType`

--- optype/numpy/_any_dtype.py
+++ optype/numpy/_any_dtype.py
@@ -3,23 +3,22 @@ The allowed `np.dtype` arguments for specific scalar types.
 The names are analogous to those in `numpy.dtypes`.
 """
 
-# pyright: reportInvalidTypeArguments=false
-
 import sys
-from typing import Protocol, TypeAlias as Alias
+from collections.abc import Sequence
+from typing import Any, TypeAlias
 
 if sys.version_info >= (3, 13):
-    from typing import Never, TypeAliasType, TypeVar
+    from typing import Never, TypeAliasType
 else:
-    from typing_extensions import Never, TypeAliasType, TypeVar
+    from typing_extensions import Never, TypeAliasType
 
 import numpy as np
 
 import optype.numpy._compat as _x
-import optype.numpy._dtype as _dt
-import optype.numpy._dtype_attr as _a
+import optype.numpy._dtype_attr as a
 import optype.numpy._scalar as _sc
-from optype._core._just import JustComplex, JustFloat, JustInt, JustObject
+from ._dtype import HasDType, ToDType as To
+from optype._core._just import Just, JustComplex, JustFloat, JustInt
 
 # ruff: noqa: RUF022
 __all__ = [
@@ -90,192 +89,165 @@ def __dir__() -> list[str]:
 
 ###
 
+b_cls: TypeAlias = type[bool]  # noqa: PYI042  # final
+i_cls: TypeAlias = type[JustInt]  # noqa: PYI042
+f_cls: TypeAlias = type[JustFloat]  # noqa: PYI042
+c_cls: TypeAlias = type[JustComplex]  # noqa: PYI042
+fc_cls: TypeAlias = f_cls | c_cls  # noqa: PYI042
+ifc_cls: TypeAlias = i_cls | fc_cls  # noqa: PYI042
 
-_SCT = TypeVar("_SCT", bound=np.generic)
-_SCT_co = TypeVar("_SCT_co", bound=np.generic, covariant=True)
-
-
-# instead of using `HasDType[np.dtype[_SCT]]`, we use this (more specific) protocol
-# for improved readability of introspection and type-checker errors
-class _HasScalarType(Protocol[_SCT_co]):
-    @property
-    def dtype(self, /) -> np.dtype[_SCT_co]: ...
-
-
-# TODO(jorenham): Export this as `onp.ToDType` (and document).
-_ToDType: Alias = type[_SCT] | np.dtype[_SCT] | _HasScalarType[_SCT]
-
-
-_IsInt: Alias = type[JustInt]
-_IsFloat: Alias = type[JustFloat]
-_IsComplex: Alias = type[JustComplex]
-_IsObject: Alias = type[JustObject]
+O_cls: TypeAlias = type[Just[object]]
+S_cls: TypeAlias = type[Just[bytes]]
+U_cls: TypeAlias = type[Just[str]]
+V_cls: TypeAlias = type[memoryview]  # final
+SU_cls: TypeAlias = S_cls | U_cls
+SUV_cls: TypeAlias = SU_cls | V_cls
 
 
 ###
 
-# bool
 
-AnyBoolDType = TypeAliasType(
-    "AnyBoolDType", type[bool] | _ToDType[_x.Bool] | _a.b1_code
-)
+# b1
+AnyBoolDType = TypeAliasType("AnyBoolDType", b_cls | To[_x.Bool] | a.b1_code)
 
-# signed integers
+# i1
+AnyInt8DType = TypeAliasType("AnyInt8DType", To[np.int8] | a.i1_code)
+AnyByteDType = AnyInt8DType  # deprecated
+# u1
+AnyUInt8DType = TypeAliasType("AnyUInt8DType", To[np.uint8] | a.u1_code)
+AnyUByteDType = AnyUInt8DType  # deprecated
+# i2
+AnyInt16DType = TypeAliasType("AnyInt16DType", To[np.int16] | a.i2_code)
+AnyShortDType = AnyInt16DType  # deprecated
+# u2
+AnyUInt16DType = TypeAliasType("AnyUInt16DType", To[np.uint16] | a.u2_code)
+AnyUShortDType = AnyUInt16DType  # deprecated
+# i4
+AnyInt32DType = TypeAliasType("AnyInt32DType", To[np.int32] | a.i4_code)
+AnyIntCDType = AnyInt32DType  # deprecated
+# u4
+AnyUInt32DType = TypeAliasType("AnyUInt32DType", To[np.uint32] | a.u4_code)
+AnyUIntCDType = AnyUInt32DType  # deprecated
+# i8
+AnyInt64DType = TypeAliasType("AnyInt64DType", To[np.int64] | a.i8_code)
+AnyLongLongDType = AnyInt64DType  # deprecated
+# u8
+AnyUInt64DType = TypeAliasType("AnyUInt64DType", To[np.uint64] | a.u8_code)
+AnyULongLongDType = AnyUInt64DType  # deprecated
+# int_ / intp / long
+if _x.NP20:
+    AnyIntPDType = TypeAliasType("AnyIntPDType", i_cls | To[np.intp] | a.i0_code)
+    AnyIntDType = AnyIntPDType
+    AnyLongDType = TypeAliasType("AnyLongDType", To[np.long] | a.l_code)
+else:
+    AnyIntPDType = TypeAliasType("AnyIntPDType", To[np.intp] | a.i0_code)
+    AnyIntDType = TypeAliasType("AnyIntDType", i_cls | To[np.int_] | a.l_code)
+    AnyLongDType = AnyIntDType
 
-AnyInt8DType = TypeAliasType("AnyInt8DType", _ToDType[np.int8] | _a.i1_code)
-AnyByteDType = AnyInt8DType
-AnyInt16DType = TypeAliasType("AnyInt16DType", _ToDType[np.int16] | _a.i2_code)
-AnyShortDType = AnyInt16DType
-AnyInt32DType = TypeAliasType("AnyInt32DType", _ToDType[np.int32] | _a.i4_code)
-AnyIntCDType = AnyInt32DType
-AnyInt64DType = TypeAliasType("AnyInt64DType", _ToDType[np.int64] | _a.i8_code)
-AnyLongLongDType = AnyInt64DType
+# uint / uintp / ulong
+AnyUIntPDType = TypeAliasType("AnyUIntPDType", To[np.uintp] | a.u0_code)
+if _x.NP20:
+    AnyULongDType = TypeAliasType("AnyULongDType", To[np.ulong] | a.L_code)
+    AnyUIntDType = AnyULongDType
+else:
+    AnyULongDType = TypeAliasType("AnyULongDType", To[np.uint] | a.L_code)
+    AnyUIntDType = AnyULongDType
 
-# unsigned integers
 
-AnyUInt8DType = TypeAliasType("AnyUInt8DType", _ToDType[np.uint8] | _a.u1_code)
-AnyUByteDType = AnyUInt8DType
-AnyUInt16DType = TypeAliasType("AnyUInt16DType", _ToDType[np.uint16] | _a.u2_code)
-AnyUShortDType = AnyUInt16DType
-AnyUInt32DType = TypeAliasType("AnyUInt32DType", _ToDType[np.uint32] | _a.u4_code)
-AnyUIntCDType = AnyUInt32DType
-AnyUInt64DType = TypeAliasType("AnyUInt64DType", _ToDType[np.uint64] | _a.u8_code)
-AnyULongLongDType = AnyUInt64DType
-
-# real floating
-
-AnyFloat16DType = TypeAliasType("AnyFloat16DType", _ToDType[np.float16] | _a.f2_code)
-AnyFloat32DType = TypeAliasType("AnyFloat32DType", _ToDType[np.float32] | _a.f4_code)
+# f2
+AnyFloat16DType = TypeAliasType("AnyFloat16DType", To[np.float16] | a.f2_code)
+# f4
+AnyFloat32DType = TypeAliasType("AnyFloat32DType", To[np.float32] | a.f4_code)
+# f8
 AnyFloat64DType = TypeAliasType(
     "AnyFloat64DType",
-    _IsFloat | _ToDType[np.float64] | _a.f8_code | None,
+    f_cls | To[np.float64] | a.f8_code | None,
 )
-AnyLongDoubleDType = TypeAliasType(
-    "AnyLongDoubleDType",
-    _ToDType[np.longdouble] | _a.g_code,
-)
-AnyFloatingDType = TypeAliasType(
-    "AnyFloatingDType",
-    _IsFloat | _ToDType[_sc.floating] | _a.fx_code,
-)
+# f10 | f12 | f16
+AnyLongDoubleDType = TypeAliasType("AnyLongDoubleDType", To[np.longdouble] | a.g_code)
 
-# complex floating
-
-AnyComplex64DType = TypeAliasType(
-    "AnyComplex64DType",
-    _ToDType[np.complex64] | _a.c8_code,
-)
+# c8
+AnyComplex64DType = TypeAliasType("AnyComplex64DType", To[np.complex64] | a.c8_code)
+# c16
 AnyComplex128DType = TypeAliasType(
     "AnyComplex128DType",
-    _IsComplex | _ToDType[np.complex128] | _a.c16_code,
+    c_cls | To[np.complex128] | a.c16_code,
 )
+# c20 | c24 | c32
 AnyCLongDoubleDType = TypeAliasType(
     "AnyCLongDoubleDType",
-    _ToDType[np.clongdouble] | _a.G_code,
-)
-AnyComplexFloatingDType = TypeAliasType(
-    "AnyComplexFloatingDType",
-    _IsComplex | _ToDType[_sc.cfloating] | _a.cx_code,
+    To[np.clongdouble] | a.G_code,
 )
 
-# temporal
-
-AnyDateTime64DType = TypeAliasType(
-    "AnyDateTime64DType",
-    _ToDType[np.datetime64] | _a.M8_code,
-)
-
+# M / N8
+AnyDateTime64DType = TypeAliasType("AnyDateTime64DType", To[np.datetime64] | a.M_code)
+# m / m8
 AnyTimeDelta64DType = TypeAliasType(
     "AnyTimeDelta64DType",
-    _ToDType[np.timedelta64] | _a.m8_code,
+    To[np.timedelta64] | a.m_code,
 )
 
 # flexible
 
-AnyBytesDType = TypeAliasType(
-    "AnyBytesDType",
-    type[bytes] | _ToDType[np.bytes_] | _a.S0_code,
-)
-AnyBytes8DType = TypeAliasType("AnyBytes8DType", _a.S1_code)
-AnyStrDType = TypeAliasType("AnyStrDType", type[str] | _ToDType[np.str_] | _a.U0_code)
-AnyCharacterDType = TypeAliasType(
-    "AnyCharacterDType",
-    type[bytes | str] | _ToDType[np.character] | _a.SU_code,
-)
+AnyBytesDType = TypeAliasType("AnyBytesDType", S_cls | To[np.bytes_] | a.S0_code)
+AnyBytes8DType = TypeAliasType("AnyBytes8DType", a.S1_code)
+AnyStrDType = TypeAliasType("AnyStrDType", S_cls | To[np.str_] | a.U0_code)
 
-# TODO: Include structured DType values, e.g. `dtype(('u8', 4))`
+_ToShape: TypeAlias = tuple[int, ...] | int
+_ToName: TypeAlias = str | tuple[str, str]
+_ToDType: TypeAlias = To[Any] | str
+_ToStructured: TypeAlias = (
+    tuple[_ToDType, _ToShape]
+    | Sequence[tuple[_ToName, _ToDType] | tuple[_ToName, _ToDType, _ToShape]]
+)
 AnyVoidDType = TypeAliasType(
     "AnyVoidDType",
-    type[memoryview] | _ToDType[np.void] | _a.V0_code,
-)
-AnyFlexibleDType = TypeAliasType(
-    "AnyFlexibleDType",
-    type[bytes | str | memoryview] | _ToDType[np.flexible] | _a.SUV_code,
+    V_cls | To[np.void] | a.V0_code | _ToStructured,
 )
 
 # object
 
-AnyObjectDType = TypeAliasType(
-    "AnyObjectDType",
-    _IsObject | _ToDType[np.object_] | _a.O_code,
-)
-AnyInexactDType = TypeAliasType(
-    "AnyInexactDType",
-    _IsFloat | _IsComplex | _ToDType[_sc.inexact] | _a.fc_code,
-)
-AnyNumberDType = TypeAliasType(
-    "AnyNumberDType",
-    _IsInt | _IsFloat | _IsComplex | _ToDType[_sc.number] | _a.uifc_code,
-)
+AnyObjectDType = TypeAliasType("AnyObjectDType", O_cls | To[np.object_] | a.O_code)
+AnyDType = TypeAliasType("AnyDType", _ToDType | _ToStructured)
 
+# abstract
 
-# NOTE: At the moment, `np.dtypes.StringDType.type: type[str]`, which is
-# impossible (i.e. `dtype[str]` isn't valid, as `str` isn't a `np.generic`)
-
-AnyULongDType = TypeAliasType("AnyULongDType", _ToDType[_x.ULong] | _a.L_code)
-AnyUIntPDType = TypeAliasType("AnyUIntPDType", _ToDType[np.uintp] | _a.u0_code)
-
-AnyUnsignedIntegerDType = TypeAliasType(
-    "AnyUnsignedIntegerDType",
-    _ToDType[_sc.uinteger] | _a.ux_code,
-)
 AnySignedIntegerDType = TypeAliasType(
     "AnySignedIntegerDType",
-    _IsInt | _ToDType[_sc.sinteger] | _a.ix_code,
+    i_cls | To[_sc.sinteger] | a.ix_code,
 )
-AnyIntegerDType = TypeAliasType(
-    "AnyIntegerDType",
-    _IsInt | _ToDType[_sc.integer] | _a.ui_code,
+AnyUnsignedIntegerDType = TypeAliasType(
+    "AnyUnsignedIntegerDType",
+    To[_sc.uinteger] | a.ux_code,
+)
+AnyFloatingDType = TypeAliasType(
+    "AnyFloatingDType",
+    f_cls | To[_sc.floating] | a.fx_code,
+)
+AnyComplexFloatingDType = TypeAliasType(
+    "AnyComplexFloatingDType",
+    c_cls | To[_sc.cfloating] | a.cx_code,
 )
 
-if _x.NP20:
-    AnyIntPDType = TypeAliasType(
-        "AnyIntPDType",
-        _IsInt | _ToDType[np.intp] | _a.i0_code,
-    )
-    AnyLongDType = TypeAliasType("AnyLongDType", _ToDType[_x.Long] | _a.l_code)
-    AnyIntDType = AnyIntPDType
-    AnyUIntDType = AnyUIntPDType
+AnyIntegerDType = TypeAliasType("AnyIntegerDType", i_cls | To[_sc.integer] | a.iu_code)
+AnyInexactDType = TypeAliasType("AnyInexactDType", fc_cls | To[_sc.inexact] | a.fc_code)
+AnyNumberDType = TypeAliasType("AnyNumberDType", ifc_cls | To[_sc.number] | a.iufc_code)
 
-    # NOTE: `np.dtypes.StringDType` didn't exist in the stubs prior to 2.1 (so
-    # I (@jorenham) added them, see https://github.com/numpy/numpy/pull/27008).
-    if _x.NP21:
-        # `numpy>=2.1`
-        _HasStringDType: Alias = _dt.HasDType[np.dtypes.StringDType]  # type: ignore[type-var]
-        AnyStringDType = TypeAliasType("AnyStringDType", _HasStringDType | _a.T_code)  # type: ignore[type-var]
-        AnyDType = TypeAliasType(
-            "AnyDType",
-            type | _ToDType[np.generic] | str | _HasStringDType,
-        )
-    else:
-        AnyStringDType = TypeAliasType("AnyStringDType", np.dtype[Never] | _a.T_code)
-        AnyDType = TypeAliasType("AnyDType", type | _ToDType[np.generic] | str)
+AnyCharacterDType = TypeAliasType(
+    "AnyCharacterDType",
+    SU_cls | To[np.character] | a.SU_code,
+)
+AnyFlexibleDType = TypeAliasType(
+    "AnyFlexibleDType",
+    SUV_cls | To[np.flexible] | a.SUV_code,
+)
+
+
+if _x.NP21:
+    # `numpy>=2.1`
+    _HasStringDType: TypeAlias = HasDType[np.dtypes.StringDType]
+    AnyStringDType = TypeAliasType("AnyStringDType", _HasStringDType | a.T_code)
+elif _x.NP20:
+    AnyStringDType = TypeAliasType("AnyStringDType", np.dtype[Never] | a.T_code)
 else:
-    # assuming that `c_void_p == c_size_t`
-    AnyIntPDType = TypeAliasType("AnyIntPDType", _ToDType[np.intp] | _a.i0_code)
-    AnyLongDType = TypeAliasType("AnyLongDType", _IsInt | _ToDType[_x.Long] | _a.l_code)
-    AnyIntDType: Alias = AnyLongDType
-    AnyUIntDType: Alias = AnyULongDType
-
     AnyStringDType = TypeAliasType("AnyStringDType", Never)
-    AnyDType = TypeAliasType("AnyDType", type | _ToDType[np.generic] | str)
