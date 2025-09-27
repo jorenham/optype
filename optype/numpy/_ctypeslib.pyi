3f0a2d73c9bdda030da553537cf88e15da8576bd
jorenham <jhammudoglu@gmail.com>
2025-09-27 04:30:06 +0200

    `numpy.ctypes`: prefer re-exporting over `TypeAliasType`

--- optype/numpy/_ctypeslib.pyi
+++ optype/numpy/_ctypeslib.pyi
@@ -1,9 +1,9 @@
 import ctypes as ct
 from ctypes import _CData as CType  # noqa: PLC2701
-from typing import Any, TypeAlias
-from typing_extensions import TypeVar
+from typing import Any
+from typing_extensions import TypeAliasType, TypeVar
 
 __all__ = "CScalar", "CType"
 
 _T = TypeVar("_T", default=Any)
-CScalar: TypeAlias = ct._SimpleCData[_T]  # noqa: SLF001
+CScalar = TypeAliasType("CScalar", ct._SimpleCData[_T], type_params=(_T,))  # noqa: SLF001
